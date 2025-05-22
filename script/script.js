486b4eb15217b662e4d5b1cd1631a2a9f80f5179
jhermand <jhermand@gmail.com>
2025-05-22 21:41:41 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,38 +1,39 @@
 function checkCode(step) {
     const input = document.getElementById('codeInput').value.trim().toUpperCase();
+    let expected = "";
     if (input === "") {
         const success = document.getElementById('success-message');
         const error = document.getElementById('error-message');
         success.style.display = "none";
         error.style.display = "none";
-        return;
-    }
-    let expected = "";
-    switch (step) {
-    case 1:
-        expected = "LIVRE";
-        break;
-    case 2:
-        expected = "LIVRE";
-        break;
-    case 3:
-        expected = "LIVRE";
-        break;
-    case 4:
-        expected = "La soile";
-        break;
-    case 5:
-        expected = "LIVRE";
-        break;
-    }
-    const success = document.getElementById('success-message');
-    const error = document.getElementById('error-message');
-
-    if (input === expected) {
-      success.style.display = "block";
-      error.style.display = "none";
     } else {
-      success.style.display = "none";
-      error.style.display = "block";
+        switch (step) {
+            case 1:
+                expected = "LIVRE";
+                break;
+            case 2:
+                expected = "LIVRE";
+                break;
+            case 3:
+                expected = "LIVRE";
+                break;
+            case 4:
+                expected = "LA SOILE";
+                break;
+            case 5:
+                expected = "LIVRE";
+                break;
+            }
+        const success = document.getElementById('success-message');
+        const error = document.getElementById('error-message');
+        if (input === expected) {
+            success.style.display = "block";
+            error.style.display = "none";
+        } else {
+            success.style.display = "none";
+            error.style.display = "block";
+        }
     }
+    
+    
   }
